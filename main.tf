59f70d07ee41f63e34b909f06c25a060942a8e62
Aba Tawiah <abatawiah@hotmail.com>
2024-02-10 16:38:04 +0000

    created vpc

---
+++ main.tf
@@ -0,0 +1,12 @@
+#VPC
+resource "aws_vpc" "VPC-Finalproject" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-vpc"
+  }
+}
+
---
+++ variable.tf
@@ -0,0 +1,7 @@
+#environment variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+#vpc variables
+variable "vpc_cidr" {}
